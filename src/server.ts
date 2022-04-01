ba9d7b8dc3e9059ebac66f2413fea3e79d52a65b
Imran Remtulla <imranr98@pm.me>
2022-04-01 14:35:12 -0400

    Added DB connection check before server start

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@
 import { app } from './app'
 import { createTransport } from './helpers/emailHelpers'
 import logger from './logger'
+import prisma from './db/prismaClient'
 
 // Verify the DB connection and email configuration, then start the server
 const startServer = async () => {
-    await (await createTransport()).verify()
+    await (await createTransport()).verify() // Make sure the email service works
+    await prisma.$connect() // Make sure the DB is connected
     app.listen(process.env.PORT || 8080, () => logger.info(`Express server launched (port ${process.env.PORT || 8080})`))
 }
 
